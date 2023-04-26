e62354d5e9e19eb683058b4c49b89086246da2db
MariuszKrol <mar_mp@interia.pl>
2023-04-26 18:15:56 +0200

    drugi commit

--- script.js
+++ script.js
@@ -8,3 +8,26 @@ function myColorChange() {
     element.style.color = "red";
 }
  
+
+function mojaFunkcja(imie) {
+
+   const clients = ["Mango", "Poly","Ajax"];
+   let message;
+   
+    for (const client of clients) {
+
+        if(client === imie) {
+            message = "Klient z takim imieniem jest juz w bazie danych!!!";
+            break;
+        }
+
+
+        message = "Nie znalezliśmy takiego klienta w bazie danych!!!";
+
+    }  
+
+       console.log(message);
+
+}       
+
+
